431af54b2d44f3c1085839b6f7a6380f19068570
Daniel Forero <daniel.forerosanchez@epfl.ch>
2021-01-31 18:11:10 +0100

    Galaxies seem to work too. Test was fitting galaxy model to void 2pcf. Missing support for extra parameters.

--- src/fit.py
+++ src/fit.py
@@ -7,16 +7,20 @@ from scipy.special import spherical_jn
 from scipy.interpolate import interp1d
 import pymultinest as pmn
 import hankl
+import os
+from mpi4py import MPI
 
 class BAO_Fitter():
     def __init__(self, plin_fn, plin_nw_fn, pnw_temp_fn=None, pratio=1, a_smooth=1,
                 k_norm=8e-3, smin=60, smax=150, sbin_size=5, kmin = 2.5e-3, kmax = 600, num_lnk_bin=2048,
                 prior_params = ((0.8, 1.2), (0, 25), (0,30)), prior_types=('flat', 'flat', 'flat'), 
-                nwalkers = 10, tolerance = 0.01, live_points = 1000, parameters = ['a', 'B', 'Snl']):
+                nwalkers = 10, tolerance = 0.01, live_points = 1000, parameters = ['a', 'B', 'Snl'], 
+                backend = 'multinest'):
 
         print(f"==> Initializing fitter.", flush=True)
+        self.backend = backend
+        
         self.parameters = parameters
-        self.backend = None
         self.prior_params = prior_params
         self.nparams = len(prior_params)
         self.prior_types = prior_types
@@ -56,10 +60,10 @@ class BAO_Fitter():
     def get_xi_fit_range(self, xi_obs, s_obs, match_bins=True):
         if match_bins:
             self.fit_range_mask = (s_obs > self.smin) & (s_obs < self.smax)
-            return xi_obs[self.fit_range_mask]
+            return np.squeeze(xi_obs[self.fit_range_mask])
         else:    
             xi_interp = interp1d(s_obs, s_obs**2*xi_obs, kind='cubic', bounds_error=True, fill_value=0)
-            return xi_interp(self.s)/self.s**2
+            return np.squeeze(xi_interp(self.s)/self.s**2)
         
         
 
@@ -82,8 +86,10 @@ class BAO_Fitter():
         
         if self.fit_range_mask is None:
             s = pd.read_csv(mocks[0], delim_whitespace=True, engine='c', names = ['s'], usecols = (0,)).values
-            self.fit_range_mask = (s > self.smin) & (s < self.smax)
-        xis = np.array([pd.read_csv(f, delim_whitespace=True, engine='c', names = ['xi0'], usecols = usecols).values[self.fit_range_mask] for f in mocks])
+            self.fit_range_mask = np.squeeze((s > self.smin) & (s < self.smax))
+
+        xis = np.squeeze(np.array([pd.read_csv(f, delim_whitespace=True, engine='c', names = ['xi0'], usecols = usecols).values[self.fit_range_mask] for f in mocks]))
+        
 
         N_mocks, N_bins = xis.shape
 
@@ -191,10 +197,11 @@ class BAO_Fitter():
         return lp + self.loglike(theta, data, s)
 
 
-    def fit(self, data, s, nsteps = 2000, backend = 'multinest', outbase="output/test_", seed=42):
-        self.backend = backend
+    def fit(self, data, s, nsteps = 100, outbase="output/test_", seed=42, resume=True):
+        
         self.nsteps = nsteps
-        if backend =='zeus':
+        
+        if self.backend =='zeus':
             param_init = []
             for i in range(self.nparams):
                 if self.prior_types[i] == 'flat':
@@ -207,17 +214,18 @@ class BAO_Fitter():
                     raise NotImplementedError
 
             start = np.array(param_init).T
-            if not os.path.isfile(outbase+"zeus_chains.txt"):
+            if not os.path.isfile(outbase+"zeus_chains.txt") or ~resume:
                 np.random.seed(seed)
                 with Pool() as pool:
-                    self.sampler = zeus.EnsembleSampler(self.nwalkers, self.nparams, self.logpost, args=[data, s], vectorize=False) # Initialise the sampler
-                    self.sampler.run_mcmc(start, nsteps) # Run sampling
-                self.sampler.summary # Print summary diagnostics
+                    sampler = zeus.EnsembleSampler(self.nwalkers, self.nparams, self.logpost, args=[data, s], vectorize=False) # Initialise the sampler
+                    sampler.run_mcmc(start, nsteps) # Run sampling
+                sampler.summary # Print summary diagnostics
+                return sampler
             else:
                 print(f"==> Chain file found, skipping the fit.")
 
-
-        elif backend == 'multinest':
+            
+        elif self.backend == 'multinest':
 
             print(f"==> Using PyMultinest. Assuming params passed are flat priors.")
 
@@ -233,22 +241,20 @@ class BAO_Fitter():
 
             
             
-            pmn.run(loglike_pmn, prior_pmn, self.nparams, outputfiles_basename=outbase, resume=True, \
+            pmn.run(loglike_pmn, prior_pmn, self.nparams, outputfiles_basename=outbase, resume=resume, \
                 verbose=True, n_live_points=self.live_points, evidence_tolerance=self.tolerance)
-
-            
-
-            
 
         else:
             raise NotImplementedError
 
-    def analyze(self, outbase = "output/test_", burnin_factor = 0.5):
+    def analyze(self, outbase = "output/test_", sampler = None, burnin_factor = 0.5, resume=True):
         if self.backend == 'zeus':
-            if not os.path.isfile(outbase+"zeus_chains.txt"):
-
+            
+            if not os.path.isfile(outbase+"zeus_chains.txt") or ~resume:
+                if sampler is None:
+                    raise ValueError
                 print(f" ==> Getting chains. thinning the chain and removing burn-in with factor {burnin_factor}")
-                chain = self.sampler.get_chain(flat=True, discard=burnin_factor, thin=10)
+                chain = sampler.get_chain(flat=True, discard=burnin_factor, thin=10)
                 np.savetxt(outbase+"zeus_chains.txt", chain)
             else:
                 chain = np.loadtxt(outbase+"zeus_chains.txt")
@@ -262,18 +268,15 @@ class BAO_Fitter():
                 q = np.diff(mcmc)
                 olist.append(q[0])
                 olist.append(q[1])
-            np.savetxt(outbase+"zeus_map.txt", np.array(olist))
-
-                
-                
-
+            self.write_line(outbase+"zeus_map.txt", np.array(olist))
 
             fig, axes = zeus.cornerplot(chain, labels=self.parameters, truth=[1, 0.75, 8], title_fmt='.4f')
-            fig.savefig("test/marginals_zeus.png")
+            fig.savefig(f"{outbase}zeus_marginals_post.png") #, bbox_inches='tight')
 
             return np.array(bestlist)
 
         elif self.backend == 'multinest':
+            print(f"==> No burnin used with Multinest backend.")
 
             res = pmn.Analyzer(outputfiles_basename=outbase, n_params=self.nparams)
             bestpar = res.get_best_fit()['parameters']
@@ -281,12 +284,14 @@ class BAO_Fitter():
             
             p = pmn.PlotMarginalModes(res)
             plt.figure(figsize=(5*self.nparams, 5*self.nparams))
-            #plt.subplots_adjust(wspace=0, hspace=0)
+            olist = []
             for i in range(self.nparams):
                 median = stats[i]['median']
                 onesigma = stats[i]['1sigma']
                 low = median - onesigma[0]
                 high = onesigma[1] - median
+                olist.append(median)
+                olist.append(stats[i]['sigma'])
                 plt.subplot(self.nparams, self.nparams, self.nparams * i + i + 1)
                 p.plot_marginal(i, with_ellipses = True, with_points = False, grid_points=50)
                 
@@ -300,9 +305,71 @@ class BAO_Fitter():
                     p.plot_conditional(i, j, with_ellipses = False, with_points = True, grid_points=30)
                     plt.xlabel(self.parameters[i])
                     plt.ylabel(self.parameters[j])
-
-            plt.savefig("output/marginals_multinest.png") #, bbox_inches='tight')
+            self.write_line(outbase+"multinest_map.txt", np.array(olist))
+            plt.savefig(f"{outbase}multinest_marginals_post.png") #, bbox_inches='tight')
             return bestpar
+
+        else: 
+            raise NotImplementedError
+
+    def fit_analyze(self, data, s, nsteps = 100, outbase="output/test_", seed=42, burnin_factor = 0.5, resume=True, plot_bf=True):
+        
+        if self.fit_range_mask is None:
+            self.get_xi_fit_range(data,s)
+        
+        sampler = self.fit(data, s, nsteps = nsteps, outbase=outbase, seed=seed, resume=resume)
+        best_parameters = self.analyze(outbase=outbase, sampler=sampler, burnin_factor=burnin_factor, resume=resume)
+        
+
+        if plot_bf:
+            best_s, best_f = self.best_fit(data, best_parameters[0], best_parameters[1], best_parameters[2], s)
+            fig, ax = plt.subplots(1, 1)
+            ax.plot(s, s**2*data, label='Data')
+            ax.plot(best_s, best_s**2*best_f, label='Best-fit')
+            ax.legend(loc=0)
+            ax.set_xlabel('$s$ [Mpc/$h$]', fontsize=13)
+            ax.set_ylabel(r'$s^2\xi_0$', fontsize=13)
+            plt.tight_layout()
+            fig.savefig(f"{outbase}bestfit.png")
+            fig.clear()
+            plt.close(fig)
+
+        return best_parameters
+
+
+    def batch_fit_analyze(self, in_file_list, nsteps, outdir, seed=42, burnin_factor=0.5, resume=True, plot_bf=True):
+        size = MPI.COMM_WORLD.Get_size()
+        rank = MPI.COMM_WORLD.Get_rank()
+        my_infile_list = np.array_split(in_file_list, size)[rank]
+        for i, f in enumerate(my_infile_list):
+            data = pd.read_csv(f, delim_whitespace=True, usecols=(0,1), names = ['s', 'xi0'])
+            basename = f"{os.path.basename(f)}"
+            ind_outdir = f"{outdir}/{basename}"
+            os.makedirs(ind_outdir, exist_ok=True)
+            
+            if len(ind_outdir) > 80 :
+                print(f"WARNING: The length of the filename is probably too long for buffer, creating symbolic links instead.", flush=True)
+                backup_odir = f"run/{basename}"
+                os.makedirs("run", exist_ok=True)
+                os.makedirs(backup_odir, exist_ok=True)
+                os.symlink(ind_outdir, backup_odir)
+                ind_outdir = backup_odir
+
+                
+            outbase = f"{ind_outdir}/{basename}_"
+
+            best_params = self.fit_analyze(data = data['xi0'].values, s = data['s'].values, nsteps=nsteps, 
+                                            outbase = outbase, seed = i + rank, burnin_factor=burnin_factor, 
+                                            resume=resume, plot_bf=plot_bf)
+
+
+        return
+
+    def write_line(self, filename, array):
+        fmt = " ".join(["%.6f"]*len(array))
+        
+        with open(filename, "w") as f:
+            f.write(fmt%tuple(array))
 
 if __name__ == '__main__':
 
@@ -325,30 +392,15 @@ if __name__ == '__main__':
     s_obs, xi_obs = np.loadtxt(args.ifile, usecols=(0,1), unpack=True)
 
     # Initialize fitter
-    bao_fitter = BAO_Fitter(plin_fn = args.plin, plin_nw_fn=args.pnw, pnw_temp_fn = args.ptemp, prior_params = ((0.8, 1.2), (0, 25), (0,30)), prior_types=('flat', 'flat', 'flat'))
+    bao_fitter = BAO_Fitter(plin_fn = args.plin, plin_nw_fn=args.pnw, pnw_temp_fn = args.ptemp, prior_params = ((0.8, 1.2), (0, 25), (0,30)), prior_types=('flat', 'flat', 'flat'), backend = 'multinest')
     # Initialize (compute) covariance matrix if necessary, else set attributes with an appropriate one
     bao_fitter.compute_covariance(mocks)
-    # Plot some tests
-    plt.imshow(bao_fitter.inv_covariance)
-    plt.savefig("test/covariance.png")
-    # Restrict imported data to fitting range
-    bao_fitter.get_xi_fit_range(xi_obs, s_obs) 
 
-    
-
-    bao_fitter.fit(xi_obs, s_obs, backend='multinest')
+    best_params = bao_fitter.fit_analyze(xi_obs, s_obs, nsteps=10, resume=True)
     # Zeus backend: 4m52s 500 steps
     # Zeus backend: 17m31s 2000 steps
     # Multinest backend 3m22s
-    best_params = bao_fitter.analyze()
-
-    # Some more tests
-    s, best = bao_fitter.best_fit(xi_obs, best_params[0], best_params[1], best_params[2], s_obs)
-    plt.clf()
-    plt.plot(s_obs, s_obs**2*xi_obs)
-    plt.plot(s, s**2*best)
-    plt.ylim(-100, 200)
-    plt.savefig("test/bestfit.png")
+    
 
 
 
